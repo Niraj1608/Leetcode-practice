d1229c6cf1c02b5f9aed5ff3a5cff6de9deeb805
Niraj Parmar <107384357+Niraj1608@users.noreply.github.com>
2024-09-18 18:49:44 +0530

    Time: 35 ms (87.65%), Space: 16.5 MB (32.99%) - LeetHub

--- 0179-largest-number/0179-largest-number.py
+++ 0179-largest-number/0179-largest-number.py
@@ -1,20 +1,19 @@
+from functools import cmp_to_key
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
-        num_strs = list(map(str, nums))
-         # Custom comparator: Compare which combination of two strings gives a larger number
-        def compare(x, y):
-            if x + y > y + x:
-                return -1  # x should come before y
-            elif x + y < y + x:
-                return 1   # y should come before x
-            else:
-                return 0   # they are equal in terms of ordering
-         # Sort the list using the custom comparator
-        num_strs.sort(key=cmp_to_key(compare))
+        nums = [str(l) for l in nums]
+                
+        def comp(num1, num2):
+            a = int(num1 + num2)
+            b = int(num2 + num1)
+            if a > b:
+                return -1
+            if b > a:
+                return 1
+            return 0
         
-        # Join the sorted strings into the largest number
-        largest_num = ''.join(num_strs)
-        
-        # Edge case: if the largest number starts with '0', return '0'
-        return '0' if largest_num[0] == '0' else largest_num
-        
+        nums = sorted(nums, key=cmp_to_key(comp))
+        res = "".join(nums)
+        while len(res) > 1 and res[0] == "0":
+            res = res[1:]
+        return res
